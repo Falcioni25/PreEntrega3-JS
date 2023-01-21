fe2bb38bc5e379abaa971aedf6648efb2db49b9d
Facundo Lescano <eflescano@gmail.com>
2023-01-21 14:08:35 -0300

    PROYECTO FINAL

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 
-productos = [
-  { id: 0, nombre: "Silla", color: "Negro", precio: 15000, imgUrl: "img/silla.jpg" },
-  { id: 1, nombre: "Mesa", color: "Nogal", precio: 150000, imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQabReG75CKxQnRzeM0KWmRh8HFKnI9O47F_izRzYg_vNOaFt51vTGsHZDSIK35O-bgHf8&usqp=CAU" },
-  { id: 2, nombre: "Sofá", color: "Gris", precio: 55000, imgUrl: "img/sofa.png" },
-  { id: 3, nombre: "Chifonnier", color: "Caoba", precio: 55000, imgUrl: "img/chifonier.jpg" },
-  { id: 4, nombre: "Velador", color: "Blanco", precio: 10000, imgUrl: "img/velador.png" },
-  { id: 5, nombre: "Cama", color: "Cedro", precio: 15000, imgUrl: "img/cama.jpg" },
-  { id: 6, nombre: "Cómoda", color: "Nogal", precio: 150000, imgUrl: "img/comoda.webp" },
-  { id: 7, nombre: "Ropero", color: "Gris", precio: 55000, imgUrl: "img/ropero.jpg" },
-  { id: 8, nombre: "Sommier", color: "Gris", precio: 55000, imgUrl: "img/Sommier.png" },
-  { id: 9, nombre: "Colchon", color: "Gris", precio: 55000, imgUrl: "img/colchon.jfif" },
-]
+fetch("data.json")
+  .then(response => response.json())
+  .then(productos => miPrograma(productos))
+
+
 //-------------------------------------------------------------------------------------------------------
+function miPrograma(productos){
 let carrito = []
 if (localStorage.getItem("carrito")) {
   carrito = JSON.parse(localStorage.getItem("carrito"))
@@ -68,6 +62,14 @@ function agregarAlCarrito(e) {
   }
   localStorage.setItem("carrito", JSON.stringify(carrito))
   renderizarCarrito(carrito)
+  Toastify({
+    text: "Producto agregado al carrito",
+    duration: 2000,
+    position: "right",
+    style: {
+      background: "linear-gradient(to right, tomato, red)",
+    }
+  }).showToast();
 }
 
 //-------------------------Creador de Carrito-------------------------------------------------  
@@ -100,19 +102,14 @@ function borrarCarrito() {
   localStorage.removeItem("carrito")
   carrito = []
   renderizarCarrito(carrito)
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Gracias por su compra!',
+    showConfirmButton: false,
+    timer: 1500
+  })
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //----------------------------Buscador-------------------------------------------------------
@@ -131,7 +128,7 @@ function renderizarProductosFiltrados() {
   let productosFiltrados = productos.filter(producto => producto.nombre.toLowerCase().toUpperCase().includes(busqueda.value.toLowercase().toUpperCase()))
   renderizarProductos(productosFiltrados)
 }
-
+}
 
 
 
